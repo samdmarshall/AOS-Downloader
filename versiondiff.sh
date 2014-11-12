fed19b46e55c73c250e8d8d93aef5fcde02a5991
samdmarshall <me@samdmarshall.com>
2014-11-12 14:05:42 -0500

    updating script

--- versiondiff.sh
+++ versiondiff.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-oldversion=`python ../fetch.py -t mac -p $1 -l | tail -2 | head -1 | awk '{print $3}'`
-folder_name="$1 $oldversion->10.10"
-mkdir $folder_name
-mv "../projects/$1.diff" $folder_name
+version_string=`python ../fetch.py -t mac -p $1 -l | tail -2 | head -1`
+oldversion=`echo $version_string | awk '{print $3}'`
+oldbuildnumber=`echo $version_string | awk '{print $1}'`
+folder_name="$1\ $oldversion->10.10"
+mkdir -p "$folder_name"
+cp "../projects/$1.diff" "$folder_name"
+touch "$folder_name/tarballs.txt"
+printf "$1 $oldversion - http://opensource.apple.com/tarballs/$1/$1-$oldbuildnumber.tar.gz\n$1 10.10 - http://opensource.apple.com/tarballs/$1/$1-$2.tar.gz" > "$folder_name/tarballs.txt"
